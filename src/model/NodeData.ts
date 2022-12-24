db0f770d21fc48738dc2d9ead873a449a05f176b
devrosch <devrosch@gmx.net>
2022-12-24 09:05:29 +0100

    add documentation

--- src/model/NodeData.ts
+++ src/model/NodeData.ts
@@ -1,7 +1,22 @@
+/**
+ * Data representing a node/fragment in the data hierarchy.
+ */
 type NodeData = {
+  /**
+   * The URL identifying this data.
+   */
   url: URL,
+  /**
+   * XY data.
+   */
   data: { x: number, y: number }[],
+  /**
+   * Meta data represented as key-value pairs.
+   */
   parameters: { key: string, value: string }[],
+  /**
+   * Child nodes/fragments.
+   */
   children: string[],
 };
 

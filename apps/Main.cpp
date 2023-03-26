29851945bf57bb7ac36d51683ff187f8065fecd8
devrosch <devrosch@gmx.net>
2023-03-26 11:50:06 +0200

    remove libjdx git submodule and move libsciwrap to subdirectory

--- apps/Main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "C++: main() executed\n";
-    // noop, required for emscripten, so that *.js and *.wasm files get
-    // geenrated for library configured as dependency see:
-    // https://stackoverflow.com/questions/34234446/cmake-is-it-possible-to-build-an-executable-from-only-static-libraries-and-no-s
-}
--- js/wasm.js
+++
@@ -1,4 +0,0 @@
-Module.onRuntimeInitialized = function() {
-    console.log('lerp result: ' + Module.lerp(1, 2, 0.5));
-}
-

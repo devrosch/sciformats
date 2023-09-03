02b49b778f0acdaaa67f42ca4c8402cfea2aa1e8
devrosch <devrosch@gmx.net>
2023-09-03 19:56:45 +0200

    implement most AnDI chromatography reading

--- lib-rs/src/andi_chrom.rs
+++ lib-rs/src/andi_chrom.rs
@@ -1,10 +1,10 @@
 use std::{error::Error, str::FromStr};
 
-use netcdf3::DataType;
+use netcdf3::{DataType, DataVector};
 
 use crate::{
     andi::{AndiDatasetCompleteness, AndiError},
-    api::{self, SciReader},
+    api::SciReader,
     FileWrapper,
 };
 
@@ -26,7 +26,23 @@ impl SciReader<FileWrapper, AndiChromFile> for AndiChromReader {
         let sample_description = AndiChromSampleDescription::new(&mut reader)?;
         let detection_method = AndiChromDetectionMethod::new(&mut reader)?;
         let raw_data = AndiChromRawData::new(&mut reader)?;
-        todo!()
+        let peak_processing_results = AndiChromPeakProcessingResults::new(
+            &mut reader,
+            &raw_data.retention_unit,
+            detection_method.detector_unit.as_deref(),
+        )?;
+        
+        Ok(AndiChromFile {
+            admin_data,
+            sample_description,
+            detection_method,
+            raw_data,
+            peak_processing_results,
+            // TODO: read
+            non_standard_variables: vec![],
+            // TODO: read
+            non_standard_attributes: vec![],
+        })
     }
 }
 
@@ -35,7 +51,7 @@ pub struct AndiChromFile {
     pub sample_description: AndiChromSampleDescription,
     pub detection_method: AndiChromDetectionMethod,
     pub raw_data: AndiChromRawData,
-    pub peak_processing_data: AndiChromPeakProcessingResults,
+    pub peak_processing_results: AndiChromPeakProcessingResults,
     pub non_standard_variables: Vec<String>,
     pub non_standard_attributes: Vec<String>,
 }
@@ -258,15 +274,15 @@ impl AndiChromDetectionMethod {
 }
 
 pub struct AndiChromRawData {
-    pub point_number: i32,                      // required
+    pub point_number: i32, // required
     pub raw_data_table_name: Option<String>,
-    pub retention_unit: String,                 // required
-    pub actual_run_time_length: f32,            // required
-    pub actual_sampling_interval: f32,          // required
-    pub actual_delay_time: f32,                 // required
-    pub ordinate_values: Vec<f32>,              // required
-    pub uniform_sampling_flag: bool,            // required?, default: true
-    pub raw_data_retention: Option<Vec<f32>>,   // required if uniformSamplingFlag==false
+    pub retention_unit: String,               // required
+    pub actual_run_time_length: f32,          // required
+    pub actual_sampling_interval: f32,        // required
+    pub actual_delay_time: f32,               // required
+    pub ordinate_values: Vec<f32>,            // required
+    pub uniform_sampling_flag: bool,          // required?, default: true
+    pub raw_data_retention: Option<Vec<f32>>, // required if uniformSamplingFlag==false
     pub autosampler_position: Option<String>,
 }
 
@@ -341,45 +357,246 @@ impl AndiChromRawData {
 }
 
 pub struct AndiChromPeakProcessingResults {
-    peak_number: i32,
-    peak_processing_results_table_name: String,
-    peak_processing_results_comments: String,
-    peak_processing_method_name: String,
-    peak_processing_date_time_stamp: String,
-    peak_amount_unit: String,
-    peaks: Vec<AndiChromPeak>,
+    pub peak_number: i32,
+    pub peak_processing_results_table_name: Option<String>,
+    pub peak_processing_results_comments: Option<String>,
+    pub peak_processing_method_name: Option<String>,
+    pub peak_processing_date_time_stamp: Option<String>,
+    pub peak_amount_unit: Option<String>,
+    pub peaks: Option<Vec<AndiChromPeak>>,
+}
+
+impl AndiChromPeakProcessingResults {
+    pub fn new(
+        reader: &mut netcdf3::FileReader,
+        peak_retention_unit: &str,
+        detector_unit: Option<&str>,
+    ) -> Result<Self, Box<dyn Error>> {
+        let peak_number_dim = reader.data_set().get_dim("peak_number");
+        let peak_number = match peak_number_dim {
+            // TODO: usize?
+            Some(dim) => dim.size() as i32,
+            None => 0,
+        };
+        let peak_processing_results_table_name = reader
+            .data_set()
+            .get_global_attr_as_string("peak_processing_results_table_name");
+        let peak_processing_results_comments = reader
+            .data_set()
+            .get_global_attr_as_string("peak_processing_results_comments");
+        let peak_processing_method_name = reader
+            .data_set()
+            .get_global_attr_as_string("peak_processing_method_name");
+        let peak_processing_date_time_stamp = reader
+            .data_set()
+            .get_global_attr_as_string("peak_processing_date_time_stamp");
+        let peak_amount_unit = reader
+            .data_set()
+            .get_global_attr_as_string("peak_amount_unit");
+        let peaks = Self::read_peaks(
+            reader,
+            peak_number,
+            peak_retention_unit,
+            peak_amount_unit.as_deref(),
+            detector_unit,
+        )?;
+
+        Ok(Self {
+            peak_number,
+            peak_processing_results_table_name,
+            peak_processing_results_comments,
+            peak_processing_method_name,
+            peak_processing_date_time_stamp,
+            peak_amount_unit,
+            peaks,
+        })
+    }
+
+    /// As the netcdf3 library (currently) does not support indexed reads, read underlying arrays as a whole and populate peak here instead of using a dedicated new().
+    fn read_peaks(
+        reader: &mut netcdf3::FileReader,
+        peak_number: i32,
+        peak_retention_unit: &str,
+        peak_amount_unit: Option<&str>,
+        detector_unit: Option<&str>,
+    ) -> Result<Option<Vec<AndiChromPeak>>, Box<dyn Error>> {
+        if peak_number <= 0 {
+            return Ok(None);
+        }
+
+        let peak_retention_time_var = read_optional_var(reader, "peak_retention_time")?;
+        // TODO: peak_name
+        let peak_amount_var = read_optional_var(reader, "peak_amount")?;
+        let peak_start_time_var = read_optional_var(reader, "peak_start_time")?;
+        let peak_end_time_var = read_optional_var(reader, "peak_end_time")?;
+        let peak_width_var = read_optional_var(reader, "peak_width")?;
+        let peak_area_var = read_optional_var(reader, "peak_area")?;
+        let peak_area_percent_var = read_optional_var(reader, "peak_area_percent")?;
+        let peak_height_var = read_optional_var(reader, "peak_height")?;
+        let peak_height_percent_var = read_optional_var(reader, "peak_height_percent")?;
+        let baseline_start_time_var = read_optional_var(reader, "baseline_start_time")?;
+        let baseline_start_value_var = read_optional_var(reader, "baseline_start_value")?;
+        let baseline_stop_time_var = read_optional_var(reader, "baseline_stop_time")?;
+        let baseline_stop_value_var = read_optional_var(reader, "baseline_stop_value")?;
+        // TODO: peak_start_detection_code
+        // TODO: peak_stop_detection_code
+        let retention_index_var = read_optional_var(reader, "retention_index")?;
+        let migration_time_var = read_optional_var(reader, "migration_time")?;
+        let peak_asymmetry_var = read_optional_var(reader, "peak_asymmetry")?;
+        let peak_efficiency_var = read_optional_var(reader, "peak_efficiency")?;
+        let mass_on_column_var = read_optional_var(reader, "mass_on_column")?;
+        let manually_reintegrated_peaks_var =
+            read_optional_var(reader, "manually_reintegrated_peaks")?;
+
+        let mut peaks = Vec::<AndiChromPeak>::new();
+        for i in 0..peak_number as usize {
+            let peak_retention_time = read_index_from_var_f32(&peak_retention_time_var, i)?;
+            // TODO: peak_name
+            let peak_amount = read_index_from_var_f32(&peak_amount_var, i)?;
+            let peak_start_time = read_index_from_var_f32(&peak_start_time_var, i)?;
+            let peak_end_time = read_index_from_var_f32(&peak_end_time_var, i)?;
+            let peak_width = read_index_from_var_f32(&peak_width_var, i)?;
+            let peak_area = read_index_from_var_f32(&peak_area_var, i)?;
+            let peak_area_percent = read_index_from_var_f32(&peak_area_percent_var, i)?;
+            let peak_height = read_index_from_var_f32(&peak_height_var, i)?;
+            let peak_height_percent = read_index_from_var_f32(&peak_height_percent_var, i)?;
+            let baseline_start_time = read_index_from_var_f32(&baseline_start_time_var, i)?;
+            let baseline_start_value = read_index_from_var_f32(&baseline_start_value_var, i)?;
+            let baseline_stop_time = read_index_from_var_f32(&baseline_stop_time_var, i)?;
+            let baseline_stop_value = read_index_from_var_f32(&baseline_stop_value_var, i)?;
+            // TODO: peak_start_detection_code
+            // TODO: peak_stop_detection_code
+            let retention_index = read_index_from_var_f32(&retention_index_var, i)?;
+            let migration_time = read_index_from_var_f32(&migration_time_var, i)?;
+            let peak_asymmetry = read_index_from_var_f32(&peak_asymmetry_var, i)?;
+            let peak_efficiency = read_index_from_var_f32(&peak_efficiency_var, i)?;
+            let mass_on_column = read_index_from_var_f32(&mass_on_column_var, i)?;
+
+            let manually_reintegrated_peaks = read_index_from_slice(
+                manually_reintegrated_peaks_var
+                    .as_ref()
+                    .map(|(_, v)| v.get_i16())
+                    .flatten(),
+                manually_reintegrated_peaks_var
+                    .as_ref()
+                    .map(|(name, _)| *name)
+                    .unwrap_or_default(),
+                i,
+            )?
+            .map(|reint| reint != &0)
+            .unwrap_or(false);
+
+            let peak = AndiChromPeak {
+                peak_retention_time,
+                // TODO: fill
+                peak_name: None,
+                peak_amount,
+                peak_start_time,
+                peak_end_time,
+                peak_width,
+                peak_area,
+                peak_area_percent,
+
+                peak_height,
+                peak_height_percent,
+                baseline_start_time,
+                baseline_start_value,
+                baseline_stop_time,
+                baseline_stop_value,
+                // TODO: fill
+                peak_start_detection_code: None,
+                // TODO: fill
+                peak_stop_detection_code: None,
+                retention_index,
+                migration_time,
+                peak_asymmetry,
+                peak_efficiency,
+                mass_on_column,
+                manually_reintegrated_peaks,
+
+                peak_retention_unit: peak_retention_unit.to_owned(),
+                peak_amount_unit: peak_amount_unit.map(|x| x.to_owned()),
+                detector_unit: detector_unit.map(|x| x.to_owned()),
+            };
+            peaks.push(peak);
+        }
+
+        Ok(Some(peaks))
+    }
+}
+
+fn read_index_from_var_f32(
+    var: &Option<(&str, DataVector)>,
+    index: usize,
+) -> Result<Option<f32>, Box<dyn Error>> {
+    let res = read_index_from_slice(
+        var.as_ref().map(|(_, v)| v.get_f32()).flatten(),
+        var.as_ref().map(|(name, _)| *name).unwrap_or_default(),
+        index,
+    )?
+    .copied();
+
+    Ok(res)
+}
+
+fn read_index_from_slice<'a, T: 'a>(
+    slice: Option<&'a [T]>,
+    var_name: &str,
+    index: usize,
+) -> Result<Option<&'a T>, Box<dyn Error>> {
+    match slice {
+        None => Ok(None),
+        Some(sl) => match sl.get(index) {
+            None => Err(Box::new(AndiError::new(&format!(
+                "Index out of bounds for {}: {}",
+                var_name, index
+            )))),
+            Some(val) => Ok(Some(val)),
+        },
+    }
+}
+
+fn read_optional_var<'a>(
+    reader: &mut netcdf3::FileReader,
+    var_name: &'a str,
+) -> Result<Option<(&'a str, DataVector)>, Box<dyn Error>> {
+    if reader.data_set().get_var(var_name).is_some() {
+        Ok(Some((var_name, reader.read_var(var_name)?)))
+    } else {
+        Ok(Option::None)
+    }
+}
+
+pub struct AndiChromPeak {
+    pub peak_retention_time: Option<f32>,
+    pub peak_name: Option<String>,
+    pub peak_amount: Option<f32>,
+    pub peak_start_time: Option<f32>,
+    pub peak_end_time: Option<f32>,
+    pub peak_width: Option<f32>,
+    pub peak_area: Option<f32>,
+    pub peak_area_percent: Option<f32>,
+    pub peak_height: Option<f32>,
+    pub peak_height_percent: Option<f32>,
+    pub baseline_start_time: Option<f32>,
+    pub baseline_start_value: Option<f32>,
+    pub baseline_stop_time: Option<f32>,
+    pub baseline_stop_value: Option<f32>,
+    pub peak_start_detection_code: Option<String>,
+    pub peak_stop_detection_code: Option<String>,
+    pub retention_index: Option<f32>,
+    pub migration_time: Option<f32>,
+    pub peak_asymmetry: Option<f32>,
+    pub peak_efficiency: Option<f32>,
+    pub mass_on_column: Option<f32>,
+    pub manually_reintegrated_peaks: bool,
+
+    pub peak_retention_unit: String,
+    pub peak_amount_unit: Option<String>,
+    pub detector_unit: Option<String>,
 }
 
 // TODO: needed?
 // pub struct AndiNonStandardVariables {}
 
 // pub struct AndiNonStandardAttributes {}
-
-pub struct AndiChromPeak {
-    peak_retention_time: f32,
-    peak_name: String,
-    peak_amount: f32,
-    peak_start_time: f32,
-    peak_end_time: f32,
-    peak_width: f32,
-    peak_area: f32,
-    peak_area_percent: f32,
-    peak_height: f32,
-    peak_height_percent: f32,
-    baseline_start_time: f32,
-    baseline_start_value: f32,
-    baseline_stop_time: f32,
-    baseline_stop_value: f32,
-    peak_start_detection_code: String,
-    peak_stop_detection_code: String,
-    retention_index: f32,
-    migration_time: f32,
-    peak_asymmetry: f32,
-    peak_efficiency: f32,
-    mass_on_column: f32,
-    manually_reintegrated_peak: bool,
-
-    peak_retention_unit: String,
-    peak_amount_unit: String,
-    detector_unit: String,
-}

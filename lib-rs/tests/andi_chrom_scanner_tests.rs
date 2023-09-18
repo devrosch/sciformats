9f8e3adf12c95b3a4a184f22836b0f83ea628381
devrosch <devrosch@gmx.net>
2023-09-18 08:21:18 +0200

    make integration tests work for wasm/node.js

--- lib-rs/tests/andi_chrom_scanner_tests.rs
+++ lib-rs/tests/andi_chrom_scanner_tests.rs
@@ -2,10 +2,12 @@ mod io;
 
 use crate::io::open_file;
 use sf_rs::{andi_chrom_scanner::AndiChromScanner, api::Scanner};
+use wasm_bindgen_test::wasm_bindgen_test;
 
 const ANDI_CHROM_VALID_FILE_PATH: &str = "andi_chrom_valid.cdf";
 const ANDI_CHROM_INVALID_FILE_PATH: &str = "dummy.cdf";
 
+#[wasm_bindgen_test]
 #[test]
 fn andi_chrom_recognize_valid_succeeds() {
     let scanner = AndiChromScanner {};
@@ -13,6 +15,7 @@ fn andi_chrom_recognize_valid_succeeds() {
     assert!(scanner.is_recognized(&valid_path, &mut valid_file));
 }
 
+#[wasm_bindgen_test]
 #[test]
 fn andi_chrom_recognize_invalid_fails() {
     let scanner = AndiChromScanner {};
--- lib-rs/tests/io/mod.rs
+++ lib-rs/tests/io/mod.rs
@@ -1,10 +1,34 @@
-use std::{fs::File, path::PathBuf};
+#[cfg(not(target_family = "wasm"))]
+use std::{
+    fs::File,
+    io::{Read, Seek},
+    path::PathBuf,
+};
 
-pub fn open_file(name: &str) -> (String, File) {
-  let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-  path.push("tests/resources/");
-  path.push(name);
-  let file = File::open(&path).unwrap();
+#[cfg(target_family = "wasm")]
+use std::io::{Cursor, Read, Seek};
 
-  (path.to_str().unwrap().to_owned(), file)
+#[cfg(not(target_family = "wasm"))]
+pub fn open_file(name: &str) -> (String, impl Read + Seek) {
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.push("tests/resources/");
+    path.push(name);
+
+    let file = File::open(&path).unwrap();
+
+    (path.to_str().unwrap().to_owned(), file)
+}
+
+#[cfg(target_family = "wasm")]
+pub fn open_file(name: &str) -> (String, impl Read + Seek) {
+    const ANDI_CHROM_VALID: &[u8] = include_bytes!("../resources/andi_chrom_valid.cdf");
+    const DUMMY: &[u8] = include_bytes!("../resources/dummy.cdf");
+
+    let file = match name {
+        "andi_chrom_valid.cdf" => Cursor::new(ANDI_CHROM_VALID),
+        "dummy.cdf" => Cursor::new(DUMMY),
+        _ => panic!(),
+    };
+
+    (name.to_owned(), file)
 }

0aa0167ecb73c8d9f78c2dceab2477506c2ef9d0
devrosch <devrosch@gmx.net>
2023-11-24 22:32:40 +0100

    improve coverage capturing

--- lib-rs/coverage.sh
+++ lib-rs/coverage.sh
@@ -11,22 +11,25 @@
 set -eu pipefail
 
 # Build with coverage instrumentation
-# export RUSTFLAGS="-Cinstrument-coverage"
+export RUSTFLAGS="-Cinstrument-coverage -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off"
 # Output file names
-# export LLVM_PROFILE_FILE="./target/coverage/sf_rs-%p-%m.profraw"
-# Use nightly Rust toolchain
-export RUSTC_BOOTSTRAP=1
+export LLVM_PROFILE_FILE="./target/debug/coverage/sf_rs-%p-%m.profraw"
 # Full build
 export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
 export RUSTDOCFLAGS="-Cpanic=abort"
+
+# Alternative approach (with currently less clear results):
+# Use nightly Rust toolchain
+# export RUSTC_BOOTSTRAP=1
+# export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+
+rm -rf ./target/debug/coverage
+mkdir -p ./target/debug/coverage
 
 cargo build
 cargo test
 
 # Uncommenting the following line and commenting out the ones below will generate an HTML report with grcov
-# grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
-rm -rf ./target/debug/coverage
-mkdir ./target/debug/coverage
+# grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing --keep-only 'src/*' -o ./target/debug/coverage/
 grcov . -s . --binary-path ./target/debug/ -t lcov --branch --ignore-not-existing --keep-only 'src/*' -o ./target/debug/coverage/lcov.info
 genhtml -o ./target/debug/coverage/ --show-details --highlight --ignore-errors source --legend ./target/debug/coverage/lcov.info

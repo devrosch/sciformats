43d667a804d6ea424136a69a2dde235183b93264
devrosch <devrosch@gmx.net>
2023-12-23 21:16:48 +0100

    parse spc new format header

--- lib-rs/tests/io/mod.rs
+++ lib-rs/tests/io/mod.rs
@@ -32,6 +32,8 @@ pub fn open_file(root_path: &str, name: &str) -> (String, impl Read + Seek) {
     const ANDI_MS_SID: &[u8] = include_bytes!("../resources/andi/andi_ms_sid.cdf");
     const SPC_NEW_FORMAT_LE: &[u8] = include_bytes!("../resources/spc/new_format_le.spc");
     const SPC_INVALID: &[u8] = include_bytes!("../resources/spc/invalid.spc");
+    const SPC_NEW_FORMAT_LE_CORRUPT: &[u8] =
+        include_bytes!("../resources/spc/new_format_le_corrupt.spc");
 
     let file = match (root_path, name) {
         ("andi", "andi_chrom_valid.cdf") => Cursor::new(ANDI_CHROM_VALID),
@@ -44,6 +46,7 @@ pub fn open_file(root_path: &str, name: &str) -> (String, impl Read + Seek) {
         ("andi", "andi_ms_sid.cdf") => Cursor::new(ANDI_MS_SID),
         ("spc", "new_format_le.spc") => Cursor::new(SPC_NEW_FORMAT_LE),
         ("spc", "invalid.spc") => Cursor::new(SPC_INVALID),
+        ("spc", "new_format_le_corrupt.spc") => Cursor::new(SPC_NEW_FORMAT_LE_CORRUPT),
         _ => panic!(),
     };
 

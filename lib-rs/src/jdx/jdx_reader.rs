4eb5d0ff95c184b29f8a0c90f3f6d3ea73897ddb
devrosch <devrosch@gmx.net>
2024-08-16 22:14:19 +0200

    add some jdx mapping

--- lib-rs/src/jdx/jdx_reader.rs
+++ lib-rs/src/jdx/jdx_reader.rs
@@ -1,10 +1,12 @@
 use super::{
-    jdx_parser::{AuditTrail, BrukerRelaxSection, BrukerSpecificParameters, JdxBlock, NTuples},
+    jdx_parser::{
+        AuditTrail, BrukerRelaxSection, BrukerSpecificParameters, JdxBlock, NTuples,
+        PeakAssignments, PeakTable,
+    },
     JdxError,
 };
 use crate::{
-    api::{Node, Reader, SeekBufRead},
-    jdx::jdx_parser::StringLdr,
+    api::{Node, Parameter, PointXy, Reader, SeekBufRead, Table},
     utils::convert_path_to_node_indices,
 };
 use std::error::Error;
@@ -29,14 +31,17 @@ impl JdxReader {
         }
     }
 
+    fn get_block_name(block: &JdxBlock<Box<dyn SeekBufRead>>) -> &str {
+        block
+            .get_ldr("TITLE")
+            .map(|ldr| ldr.value.as_str())
+            .unwrap_or_default()
+    }
+
     fn retrieve_node(&self, node_indices: &[usize]) -> Result<Node, JdxError> {
         let generate_illegal_path_error =
             |node_index: usize, block: &JdxBlock<Box<dyn SeekBufRead>>| -> JdxError {
-                let block_title = if let Some(title) = block.get_ldr("TITLE") {
-                    title.value.as_str()
-                } else {
-                    ""
-                };
+                let block_title = Self::get_block_name(block);
                 JdxError::new(&format!(
                     "Illegal path for reading node. Block: \"{}\", child index: {}",
                     block_title, node_index
@@ -120,13 +125,37 @@ impl JdxReader {
     }
 
     fn map_bruker_relax_section(section: &BrukerRelaxSection) -> Result<Node, JdxError> {
-        todo!()
+        let name = section.name.clone();
+        // todo: use different param type once available
+        let parameters = vec![Parameter::from_str_str("", &section.content)];
+
+        Ok(Node {
+            name,
+            parameters,
+            data: vec![],
+            metadata: vec![],
+            table: None,
+            child_node_names: vec![],
+        })
     }
 
     fn map_bruker_specific_parameters(
         section: &BrukerSpecificParameters,
     ) -> Result<Node, JdxError> {
-        todo!()
+        let name = section.name.clone();
+        let mut parameters = vec![];
+        for ldr in &section.content {
+            parameters.push(Parameter::from_str_str(&ldr.label, &ldr.value));
+        }
+
+        Ok(Node {
+            name,
+            parameters,
+            data: vec![],
+            metadata: vec![],
+            table: None,
+            child_node_names: vec![],
+        })
     }
 
     fn map_n_tuples(
@@ -138,7 +167,12 @@ impl JdxReader {
     }
 
     fn is_peak_data(block: &JdxBlock<Box<dyn SeekBufRead>>) -> bool {
-        todo!()
+        let data_type = block
+            .get_ldr("DATATYPE")
+            .map(|ldr| ldr.value.as_str())
+            .unwrap_or_default()
+            .to_lowercase();
+        data_type == "mass spectrum"
     }
 
     fn map_audit_trail(audit_trail: &AuditTrail<Box<dyn SeekBufRead>>) -> Result<Node, JdxError> {
@@ -149,6 +183,85 @@ impl JdxReader {
         block: &JdxBlock<Box<dyn SeekBufRead>>,
         is_peak_data: bool,
     ) -> Result<Node, JdxError> {
+        let name = Self::get_block_name(block).to_owned();
+
+        let mut parameters = Vec::<Parameter>::new();
+        for ldr in &block.ldrs {
+            parameters.push(Parameter::from_str_str(
+                ldr.label.as_str(),
+                ldr.value.as_str(),
+            ));
+        }
+
+        let mut data = Self::map_data(block)?;
+
+        let table = if let Some(peak_assignments) = &block.peak_assignments {
+            Some(Self::map_peak_assignments(peak_assignments)?)
+        } else if let Some(peak_table) = &block.peak_table {
+            let table = Self::map_peak_table(peak_table)?;
+            if is_peak_data && data.is_empty() {
+                // for MS, map peak table as data if no other data is present
+                data = Self::map_peak_table_as_data(peak_table)?;
+            }
+            Some(table)
+        } else {
+            None
+        };
+
+        let mut child_node_names = Vec::<String>::new();
+        for bruker_relax_section in &block.bruker_relax_sections {
+            child_node_names.push(bruker_relax_section.name.clone());
+        }
+        for bruker_specific_section in &block.bruker_specific_parameters {
+            child_node_names.push(bruker_specific_section.name.clone());
+        }
+        if let Some(n_tuples) = &block.n_tuples {
+            // consider NTUPLES LDR as child node
+            child_node_names.push(n_tuples.data_form.clone());
+        }
+        if block.audit_trail.is_some() {
+            // consider AUDIT TRAIL LDR as child node
+            child_node_names.push("AUDITTRAIL".to_owned());
+        }
+        for block in &block.blocks {
+            child_node_names.push(Self::get_block_name(block).to_owned());
+        }
+
+        let metadata = Self::map_block_metadata(block)?;
+
+        Ok(Node {
+            name,
+            parameters,
+            data,
+            metadata,
+            table,
+            child_node_names,
+        })
+    }
+
+    fn map_data(block: &JdxBlock<Box<dyn SeekBufRead>>) -> Result<Vec<PointXy>, JdxError> {
+        todo!()
+    }
+
+    fn map_peak_assignments(
+        block: &PeakAssignments<Box<dyn SeekBufRead>>,
+    ) -> Result<Table, JdxError> {
+        todo!()
+    }
+
+    fn map_peak_table(block: &PeakTable<Box<dyn SeekBufRead>>) -> Result<Table, JdxError> {
+        todo!()
+    }
+
+    fn map_peak_table_as_data(
+        block: &PeakTable<Box<dyn SeekBufRead>>,
+    ) -> Result<Vec<PointXy>, JdxError> {
+        todo!()
+    }
+
+    fn map_block_metadata(
+        block: &JdxBlock<Box<dyn SeekBufRead>>,
+    ) -> Result<Vec<(String, String)>, JdxError> {
         todo!()
     }
 }

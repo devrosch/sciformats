de7d2ef5d947da4d5e78acaf5d769485ce1587a7
devrosch <devrosch@gmx.net>
2024-09-02 23:16:00 +0200

    fix Rust outdated check

--- lib-rs/check_updates.sh
+++ lib-rs/check_updates.sh
@@ -7,7 +7,7 @@
 
 set -eu pipefail
 
-num_outdated_packages="$(cargo update --dry-run 2>&1 >/dev/null | grep -Po '(?<=Locking ).*(?= packages to latest compatible versions)')"
+num_outdated_packages="$(cargo update --dry-run 2>&1 >/dev/null | { grep -Po '(?<=Locking ).*(?= packages to latest compatible versions)' || echo 0; } )"
 
 if [ "$num_outdated_packages" -gt 0 ]; then
   echo "Number of outdated packages: $num_outdated_packages"

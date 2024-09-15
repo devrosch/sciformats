bcb34dbfdf44eaba2fe03073801124a70050c1d0
devrosch <devrosch@gmx.net>
2024-09-15 21:24:54 +0200

    add generic dialog component

---
+++ web-ui/src/components/Dialog.test.ts
@@ -0,0 +1,77 @@
+/* eslint-disable no-duplicate-imports */
+import './Dialog'; // for side effects
+import Dialog from './Dialog';
+
+const element = 'sf-dialog';
+
+const showModal = jest.fn();
+const close = jest.fn();
+
+beforeAll(() => {
+  // see: https://github.com/jsdom/jsdom/issues/3294
+  HTMLDialogElement.prototype.showModal = showModal;
+  HTMLDialogElement.prototype.close = close;
+});
+
+beforeEach(() => {
+  // see: https://github.com/jsdom/jsdom/issues/3294
+  showModal.mockClear();
+  close.mockClear();
+});
+
+afterEach(() => {
+  // make sure disconnectedCallback() is called during test
+  document.body.innerHTML = '';
+});
+
+test('sf-dialog renders', async () => {
+  document.body.innerHTML = `<${element}/>`;
+  const dialog = document.body.querySelector(element) as Dialog;
+  expect(dialog).toBeTruthy();
+
+  const htmlDialog = dialog.querySelector('dialog') as HTMLDialogElement;
+  expect(htmlDialog).toBeTruthy();
+});
+
+test('sf-dialog showMessage() open dialog with message', async () => {
+  document.body.innerHTML = `<${element}/>`;
+  const dialog = document.body.querySelector(element) as Dialog;
+  expect(dialog).toBeTruthy();
+  const htmlDialog = document.querySelector('dialog') as HTMLDialogElement;
+  expect(htmlDialog).toBeTruthy();
+
+  expect(dialog.hasAttribute('open')).toBeFalsy();
+  expect(htmlDialog.hasAttribute('open')).toBeFalsy();
+  expect(showModal).toHaveBeenCalledTimes(0);
+  expect(close).toHaveBeenCalledTimes(0);
+  dialog.showMessage('Test message.');
+  // dialog.showModal(true);
+  htmlDialog.setAttribute('open', ''); // mock showModel() does not set open attribute
+  expect(dialog.hasAttribute('open')).toBeTruthy();
+  expect(showModal).toHaveBeenCalledTimes(1);
+  expect(close).toHaveBeenCalledTimes(0);
+  expect(htmlDialog.textContent?.includes('Test message.')).toBeTruthy();
+  dialog.showModal(false);
+  expect(showModal).toHaveBeenCalledTimes(1);
+  expect(close).toHaveBeenCalledTimes(1);
+});
+
+test('sf-dialog clicking anywhere closes dialog', async () => {
+  document.body.innerHTML = `<${element}/>`;
+  const dialog = document.body.querySelector(element) as Dialog;
+  expect(dialog).toBeTruthy();
+  const htmlDialog = document.querySelector('dialog') as HTMLDialogElement;
+  expect(htmlDialog).toBeTruthy();
+
+  const mouseEvent = {
+    target: htmlDialog,
+    stopPropagation: jest.fn(),
+  } as unknown as MouseEvent;
+
+  dialog.showModal(true);
+  htmlDialog.setAttribute('open', ''); // mock showModel() does not set open attribute
+
+  expect(close).toHaveBeenCalledTimes(0);
+  dialog.handleOutsideSelection(mouseEvent);
+  expect(close).toHaveBeenCalledTimes(1);
+});

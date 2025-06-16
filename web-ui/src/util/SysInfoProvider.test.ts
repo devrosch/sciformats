35f9927191a1eb2ee86dff38ad10a29cbc362d91
devrosch <devrosch@gmx.net>
2025-06-16 20:58:42 +0200

    bump all jest related dependencies

--- web-ui/src/util/SysInfoProvider.test.ts
+++ web-ui/src/util/SysInfoProvider.test.ts
@@ -1,10 +1,20 @@
 import SysInfoProvider from './SysInfoProvider';
 
-let windowSpy: jest.SpyInstance | null = null;
+let navigatorSpy: jest.SpyInstance | null = null;
 
 // for details, see: https://stackoverflow.com/questions/41885841/how-can-i-mock-the-javascript-window-object-using-jest
 beforeEach(() => {
-  windowSpy = jest.spyOn(global, 'window', 'get');
+  // work around issue introduced with jest@30.
+  // Related: https://github.com/jestjs/jest/issues/15674
+  // https://jestjs.io/blog#known-issues
+  // https://stackoverflow.com/a/56769293
+  Object.defineProperty(window, 'navigator', {
+    configurable: true,
+    get() {
+      return {};
+    },
+  });
+  navigatorSpy = jest.spyOn(window, 'navigator', 'get');
 });
 
 afterEach(() => {
@@ -23,13 +33,11 @@ it('returns one of the defined strings for the OS', () => {
 });
 
 it('detects Mac OS with Firefox', () => {
-  windowSpy?.mockImplementation(() => ({
-    navigator: {
-      oscpu: 'Intel Mac OS X 10.15',
-      platform: 'MacIntel',
-      userAgent:
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:103.0) Gecko/20100101 Firefox/103.0',
-    },
+  navigatorSpy?.mockImplementation(() => ({
+    oscpu: 'Intel Mac OS X 10.15',
+    platform: 'MacIntel',
+    userAgent:
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:103.0) Gecko/20100101 Firefox/103.0',
   }));
 
   const os = SysInfoProvider.detectOS();
@@ -38,13 +46,11 @@ it('detects Mac OS with Firefox', () => {
 });
 
 it('detects Mac OS with Safari', () => {
-  windowSpy?.mockImplementation(() => ({
-    navigator: {
-      // no oscpu entry
-      platform: 'MacIntel',
-      userAgent:
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15',
-    },
+  navigatorSpy?.mockImplementation(() => ({
+    // no oscpu entry
+    platform: 'MacIntel',
+    userAgent:
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15',
   }));
 
   const os = SysInfoProvider.detectOS();
@@ -53,13 +59,11 @@ it('detects Mac OS with Safari', () => {
 });
 
 it('detects Mac OS with Chrome', () => {
-  windowSpy?.mockImplementation(() => ({
-    navigator: {
-      // no oscpu entry
-      platform: 'MacIntel',
-      userAgent:
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36',
-    },
+  navigatorSpy?.mockImplementation(() => ({
+    // no oscpu entry
+    platform: 'MacIntel',
+    userAgent:
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36',
   }));
 
   const os = SysInfoProvider.detectOS();
@@ -68,13 +72,11 @@ it('detects Mac OS with Chrome', () => {
 });
 
 it('detects Windows with Chrome', () => {
-  windowSpy?.mockImplementation(() => ({
-    navigator: {
-      // no oscpu entry
-      platform: 'Win32',
-      userAgent:
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
-    },
+  navigatorSpy?.mockImplementation(() => ({
+    // no oscpu entry
+    platform: 'Win32',
+    userAgent:
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
   }));
 
   const os = SysInfoProvider.detectOS();
@@ -83,13 +85,11 @@ it('detects Windows with Chrome', () => {
 });
 
 it('detects Windows with Edge', () => {
-  windowSpy?.mockImplementation(() => ({
-    navigator: {
-      // no oscpu entry
-      platform: 'Win32',
-      userAgent:
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.36 Safari/537.36 Edg/104.0.1293.54',
-    },
+  navigatorSpy?.mockImplementation(() => ({
+    // no oscpu entry
+    platform: 'Win32',
+    userAgent:
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.36 Safari/537.36 Edg/104.0.1293.54',
   }));
 
   const os = SysInfoProvider.detectOS();
@@ -98,13 +98,11 @@ it('detects Windows with Edge', () => {
 });
 
 it('detects Windows with Firefox', () => {
-  windowSpy?.mockImplementation(() => ({
-    navigator: {
-      oscpu: 'Windows NT 6.1; Win64; x64',
-      platform: 'Win32',
-      userAgent:
-        'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0',
-    },
+  navigatorSpy?.mockImplementation(() => ({
+    oscpu: 'Windows NT 6.1; Win64; x64',
+    platform: 'Win32',
+    userAgent:
+      'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0',
   }));
 
   const os = SysInfoProvider.detectOS();
@@ -113,13 +111,11 @@ it('detects Windows with Firefox', () => {
 });
 
 it('detects Linux/Unix with Firefox', () => {
-  windowSpy?.mockImplementation(() => ({
-    navigator: {
-      oscpu: 'Linux x86_64',
-      platform: 'Linux x86_64',
-      userAgent:
-        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv: 79.0) Gecko/20100101 Firefox/79.0',
-    },
+  navigatorSpy?.mockImplementation(() => ({
+    oscpu: 'Linux x86_64',
+    platform: 'Linux x86_64',
+    userAgent:
+      'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv: 79.0) Gecko/20100101 Firefox/79.0',
   }));
 
   const os = SysInfoProvider.detectOS();
@@ -128,12 +124,10 @@ it('detects Linux/Unix with Firefox', () => {
 });
 
 it('detects Linux/Unix with Chromium', () => {
-  windowSpy?.mockImplementation(() => ({
-    navigator: {
-      platform: 'Linux x86_64',
-      userAgent:
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/79.0.3945.130 Chrome/79.0.3945.130 Safari/537.36',
-    },
+  navigatorSpy?.mockImplementation(() => ({
+    platform: 'Linux x86_64',
+    userAgent:
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/79.0.3945.130 Chrome/79.0.3945.130 Safari/537.36',
   }));
 
   const os = SysInfoProvider.detectOS();
@@ -142,12 +136,10 @@ it('detects Linux/Unix with Chromium', () => {
 });
 
 it('detects Linux/Unix with Chrome', () => {
-  windowSpy?.mockImplementation(() => ({
-    navigator: {
-      platform: 'Linux x86_64', // assumption, no actually checked
-      userAgent:
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
-    },
+  navigatorSpy?.mockImplementation(() => ({
+    platform: 'Linux x86_64', // assumption, no actually checked
+    userAgent:
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
   }));
 
   const os = SysInfoProvider.detectOS();

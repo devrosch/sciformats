1bf077bbf1aab56650d2b5cf15e71d665b602b34
devrosch <59132596+devrosch@users.noreply.github.com>
2025-10-18 17:37:35 +0200

    fix web build issue after bumping rs dependencies

--- web-ui/src/worker/WorkerRsInternalUtils.test.ts
+++ web-ui/src/worker/WorkerRsInternalUtils.test.ts
@@ -59,6 +59,7 @@ const mockReader: Reader = {
     table: {},
     childNodeNames: [],
     free: jest.fn(),
+    [Symbol.dispose]: jest.fn(),
   })),
   getExportFormats: jest.fn(() => ['Json']),
   /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
@@ -67,6 +68,7 @@ const mockReader: Reader = {
     return blob;
   }),
   free: jest.fn(),
+  [Symbol.dispose]: jest.fn(),
 };
 
 const fileInfoStub: WorkerFileInfo = {
@@ -172,6 +174,7 @@ test('onOpen() returns error if exception occurs', async () => {
       throw new Error('getReader() error');
     }),
     free: jest.fn(),
+    [Symbol.dispose]: jest.fn(),
   };
   const openFiles = new Map<string, Reader>();
 
